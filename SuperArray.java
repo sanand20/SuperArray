b8ebd0f6b467fabbd18bd45ac43bfd01d6d37e36
Shriya <sanand20@stuy.edu>
2020-11-03 19:08:47 -0500

    done with a,b,c

---
+++ SuperArray.java
@@ -0,0 +1,12 @@
+public class SuperArray{
+  private String [] data;
+  private int size;
+
+  public SuperArray() {
+    data = new String [10];
+    size = 0;
+  }
+  public int size(){
+    return size;
+}
+}
---
+++ Tester.java
@@ -0,0 +1,6 @@
+public class Tester{
+  public static void main(String[]args){
+    SuperArray words = new SuperArray();
+    System.out.println(words.size());
+}
+}
